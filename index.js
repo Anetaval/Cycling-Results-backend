117f7bde19c6b34826765c0920036602e5306df4
Aneta Valkova <anetka@Aneta-MacBook-Air.local>
2025-03-13 09:13:17 +0100

    🐛 Oprava dvojitého importu cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const cors = require('cors');
+
 
 const allowedOrigins = [
   'http://localhost:3000', // pro vývoj
